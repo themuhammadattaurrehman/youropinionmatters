a1ab80445457976ceb62705764b657205e3fc8dd
Attaurrehman-bop <73884338+themuhammadattaurrehman@users.noreply.github.com>
2024-01-02 19:53:00 +0500

    percentage okay and also changes in footer

--- quiz.php
+++ quiz.php
@@ -3,7 +3,7 @@
 include 'connection.php';
 
 // Retrieve questions from the database
-$sql = "SELECT * FROM question where quiz=1";
+$sql = "SELECT *  FROM question where quiz=1";
 $result = $conn->query($sql);
 
 $conn->close();
@@ -41,30 +41,40 @@ $conn->close();
             <div class="card-body">
               <h5 class="card-title">Insert Question</h5>
               <?php
-              if ($result->num_rows > 0) {
-                echo '<form method="post" action="vote_handler.php">';
-                while ($row = $result->fetch_assoc()) {
+             if ($result->num_rows > 0) {
+              echo '<form method="post" action="vote_handler.php">';
+              while ($row = $result->fetch_assoc()) {
                   echo '<div class="card mb-3">';
                   echo '<div class="card-body">';
                   echo '<h5 class="card-title">Question: ' . $row["question"] . '</h5>';
                   echo '<input type="hidden" name="question_id[]" value="' . $row["id"] . '">';
-
+          
+                  // Calculate the sum of op1, op2, op3, and op4
+                  $sum = $row['op1_votes'] + $row['op2_votes'] + $row['op3_votes'] + $row['op4_votes'];
+          
+                  // Display the sum
+                  echo '<p>Sum of Options: ' . $sum . '</p>';
+          
                   // Loop through options and create radio buttons
                   for ($j = 1; $j <= 4; $j++) {
-                    $optionKey = 'op' . $j;
-                    $optionsKey = 'op' . $j.'_votes';
-                    echo '<label>';
-                    echo '<input type="radio" name="option[' . $row["id"] . ']" value="' . $optionKey . '"> ' . $row[$optionKey] . '(Votes: ' . $row[$optionsKey] . ')</label><br>';
+                      $optionKey = 'op' . $j;
+                      $optionsKey = 'op' . $j . '_votes';
+                      if($sum!==0){
+                      $percentage = ($row[$optionsKey] /$sum) * 100;
+                    }
+                      echo '<label>';
+                      echo '<input type="radio" name="option[' . $row["id"] . ']" value="' . $optionKey . '"> ' . $row[$optionKey] . ' (Votes: ' . $percentage . ')</label><br>';
                   }
-
+          
                   echo '</div>';
                   echo '</div>';
-                }
-                echo '<input type="submit" value="Vote">';
-                echo '</form>';
-              } else {
-                echo '<p>No questions available.</p>';
               }
+              echo '<input type="submit" value="Vote">';
+              echo '</form>';
+          } else {
+              echo '<p>No questions available.</p>';
+          }
+          
               ?>
 
             </div>
